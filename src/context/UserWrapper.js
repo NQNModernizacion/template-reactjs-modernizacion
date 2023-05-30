5f6bca5b5acc537b983210e9cc62a8d3d5b8c250
santiagoScantamburlo <santiago.scantamburlo@est.fi.uncoma.edu.ar>
2023-05-30 11:23:17 -0300

    Cambios faltantes UserWrapper

--- src/context/UserWrapper.js
+++ src/context/UserWrapper.js
@@ -1,53 +1,58 @@
 import React, { useState } from "react";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 
-import { hasRole, hasPermission, reloadSesion } from './handlers';
+import {
+  getInitialState,
+  hasPermission,
+  hasRole,
+  reloadSesion,
+} from "./handlers";
 
-import { Modal } from '../components'
-import { intervalSession, logout, setSession } from "../utils/sessionStorage";
 import { useEffect } from "react";
+import { Modal } from "../components";
+import { intervalSession, logout, setSession } from "../utils/sessionStorage";
 
 export const UserContext = React.createContext(null);
 
 export const UserWrapper = ({ children }) => {
+  const [store, setStore] = useState({ ...getInitialState() });
 
-    const [store, setStore] = useState({
-        loading: true,
-        sesionModal: false,
-        data: null,
-    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => intervalSession(actions), []);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => intervalSession(actions), [])
+  const actions = {
+    setUser: (data) => setStore({ ...store, data, loading: false }),
+    setLoading: (bool) => setStore({ ...store, loading: bool }),
+    setSesionModal: (bool) => setStore({ ...store, sesionModal: bool }),
 
-    const actions = {
-        setUser: (data) => setStore({ ...store, data, loading: false }),
-        setLoading: (bool) => setStore({ ...store, loading: bool }),
+    getPerfil: () => store.data,
 
-        getPerfil: () => store.data,
+    hasRole: (role) => hasRole(role, store.user),
+    hasPermission: (permission) => hasPermission(permission, store.user),
+  };
 
-        hasRole: (role) => hasRole(role, store.user),
-        hasPermission: (permission) => hasPermission(permission, store.user),
-    }
+  /** Por cada update del state actualizamos la sesion */
+  setSession(store.data);
 
-    /** Por cada update del state actualizamos la sesion */
-    setSession(store.data);
+  return (
+    <UserContext.Provider value={{ store, actions, loading: store.loading }}>
+      {children}
+      <ToastContainer />
 
-    return (
-        <UserContext.Provider value={{ store, actions, loading: store.loading }}>
-            {children}
-            <ToastContainer />
-
-            <Modal
-                styles={{ header: { backgroundColor: '#1766ad', color: 'white' } }}
-                size={'md'}
-                show={store.sesionModal}
-                setShow={() => logout()}
-                title={() => 'SU SESION ESTA POR CADUCAR'}
-            >
-                <button className="btn btn-primary w-100" onClick={() => reloadSesion(setStore)}>RECARGAR SESION</button>
-            </Modal>
-
-        </UserContext.Provider>
-    );
-}
+      <Modal
+        styles={{ header: { backgroundColor: "#1766ad", color: "white" } }}
+        size={"md"}
+        show={store.sesionModal}
+        setShow={() => logout()}
+        title={() => "SU SESION ESTA POR CADUCAR"}
+      >
+        <button
+          className="btn btn-primary w-100"
+          onClick={() => reloadSesion(setStore)}
+        >
+          RECARGAR SESION
+        </button>
+      </Modal>
+    </UserContext.Provider>
+  );
+};
--- src/context/handlers.js
+++ src/context/handlers.js
@@ -1,58 +1,81 @@
-import { axios } from "../utils/axios"
-import { logout } from "../utils/sessionStorage"
+import { axios } from "../utils/axios";
+import { logout } from "../utils/sessionStorage";
+
+export const getInitialState = () => {
+  const com = {
+    loading: false,
+    sesionModal: false,
+  };
+
+  const sesion = getSession();
+  let initialState;
+
+  if (sesion) {
+    initialState = {
+      ...com,
+      data: { ...sesion },
+    };
+  } else {
+    initialState = {
+      ...com,
+      data: null,
+    };
+  }
+
+  return initialState;
+};
 
 /** Verifica si existe el rol en la coleccion del usuario */
 export const hasRole = (role, user) => {
-    if (!role) return false
-    if (!user) return false
+  if (!role) return false;
+  if (!user) return false;
 
-    return user.roles?.some(r => r.name === role)
-}
+  return user.roles?.some((r) => r.name === role);
+};
 
 export const hasPermission = (permission, user) => {
+  if (!permission) return false;
+  if (!user) return false;
 
-    if (!permission) return false
-    if (!user) return false
+  if (user.permissions?.some((p) => p.name === permission)) {
+    return true;
+  }
 
-    if (user.permissions?.some(p => p.name === permission)) {
-        return true;
-    }
+  /* Generamos un arreglo nuevo de permisos de esos roles */
+  const permissionsRoles = user.roles?.reduce((prev, curr) => {
+    prev.push(...curr.permissions);
+    return prev;
+  }, []);
 
-    /* Generamos un arreglo nuevo de permisos de esos roles */
-    const permissionsRoles = user.roles?.reduce((prev, curr) => {
-        prev.push(...curr.permissions)
-        return prev
-    }, [])
-
-    return permissionsRoles?.some(p => p.name === permission)
-}
+  return permissionsRoles?.some((p) => p.name === permission);
+};
 
 export const hasDirectPermission = (permission, user) => {
-    if (!permission) return false
-    if (!user) return false
+  if (!permission) return false;
+  if (!user) return false;
 
-    return user.permissions?.some(p => p.name === permission)
-}
+  return user.permissions?.some((p) => p.name === permission);
+};
 
 export const reloadSesion = async (setStore) => {
-    setStore((store) => ({ ...store, loading: true }))
+  setStore((store) => ({ ...store, loading: true }));
 
-    const response = await axios().post('refresh')
-    const { data, error } = response.data
+  const response = await axios().post("refresh");
+  const { data, error } = response.data;
 
-    if (data) {
-        setStore((store) => ({
-            ...store,
-            loading: false,
-            sesionModal: false,
-            data: {
-                ...store.data,
-                ...data
-            }
-        }))
-    }
+  if (data) {
+    setStore((store) => ({
+      ...store,
+      loading: false,
+      sesionModal: false,
+      data: {
+        ...store.data,
+        ...data,
+      },
+    }));
+  }
 
-    if (error) {
-        logout()
-    }
-}
+  if (error) {
+    logout();
+  }
+};
