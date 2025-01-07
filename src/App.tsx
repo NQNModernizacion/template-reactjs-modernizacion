b199c831b5007dab318a99fd5d009673fa6cb030
AbigailCo <abigail.candelaria.corrales@gmail.com>
2025-01-07 13:26:30 -0300

    Se creo la estructura "routes":[   "RouteProvider.tsx",       []     ],

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from "react"
-import { HashRouter, Route, Routes } from "react-router-dom"
+// import { HashRouter, Route, Routes } from "react-router-dom"
 
-import { NotFound } from "./components"
+// import { NotFound } from "./components"
 
 import { UserContext } from "./context/UserWrapper"
 import  RouteProvider  from "./router/RouteProvider"
@@ -18,31 +18,7 @@ const App = () => {
     console.log(__DEV__)
 
     return (
-        <HashRouter>
-            <Routes>
-                <Route element={<L.UserLayout />}>
-                    <Route path='/' element={<S.Menu />} />
-
-                    {/*  <Route element={<Management />} path="/administrador/roles-permisos" />
-          <Route element={<ManagementRoles />} path="/administrador/roles-permisos/roles" />
-          <Route element={<ManagementPermisos />} path="/administrador/roles-permisos/permisos" />
-          <Route
-            element={<ManagementRolesPermisos />}
-            path="/administrador/roles-permisos/role-permisos"
-          />
-          <Route element={<ManagementActividad />} path="/administrador/activity-log" />
-          <Route element={<ManagementUsuarios />} path="/administrador/roles-permisos/usuarios" />
- */}
-                    {__DEV__ && (
-                        /* Esto solo se visualiza en desarrollo */
-                        <Route path='/_viewcom' element={<S.DevScreen />} />
-                    )}
-
-                    <Route path='*' element={<NotFound />} />
-                </Route>
-                <Route path='/login' element={<S.Login />} />
-            </Routes>
-        </HashRouter>
+        <RouteProvider/>
     )
 }
 
